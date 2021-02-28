302d39ed896a082883d0de73ed524e7e8e496674
lorenmarinucci <lorenmarinucci@gmail.com>
2021-02-27 22:04:17 -0300

    Entities added

--- src/main/java/spring5webapp/Spring5webappApplication.java
+++ src/main/java/spring5webapp/Spring5webappApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.springwebapp;
+package spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
